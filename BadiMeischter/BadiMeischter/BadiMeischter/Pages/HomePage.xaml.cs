0c89bb52d662cb5b7b7a7bedb7c17155fdc85504
Luca Marti <luca.marti@bluewin.ch>
2017-05-30 13:17:59 +0200

    Bug fix

--- BadiMeischter/BadiMeischter/BadiMeischter/Pages/HomePage.xaml.cs
+++ BadiMeischter/BadiMeischter/BadiMeischter/Pages/HomePage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using BadiMeischter.Model;
 using Newtonsoft.Json;
